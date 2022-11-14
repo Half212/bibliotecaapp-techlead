a42aff3b97e6f9b90be8883265b0c0964e1372b2
Aquiles Leite <53454449+Half212@users.noreply.github.com>
2022-11-14 15:06:10 -0300

    ss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURLBase: 'https://biblioteca-backend-techlead.herokuapp.com',
+  apiURLBase: 'https://bibliotecapp-backend.herokuapp.com/',
   clientId: 'my-angular-app',
   clientSecret: '@321',
   obterTokenUrl: '/oauth/token'
